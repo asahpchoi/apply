a3ba4682485421eaa560a92d6c8f1ead92822030
Asa HP Choi <asa_hp_choi@manulife.com>
2018-09-04 14:23:13 +0800

    X

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoreSvcService } from './core/core/core-svc.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title;
+  constructor(svc: CoreSvcService) {
+    this.title = svc.getTitle();
+  }
 }
