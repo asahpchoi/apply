6362857688be472bec3aa9b89f47f293ed3378f6
Asa HP Choi <asa_hp_choi@manulife.com>
2018-09-04 14:00:40 +0800

    modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {CoreModule} from './core/core/core.module';
+import {SharedModule} from './core/shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
